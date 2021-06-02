3a88eaee6519a2d29646279f198e3b43e118cb3e
Aaron  Parodi Paez <paezparodi@gmail.com>
2021-06-02 13:11:54 -0500

    segunda prueba

--- automatizacionlinkedin.py
+++ automatizacionlinkedin.py
@@ -1,2 +1,9 @@
-nombre = "aaron"
-print("Hola " + nombre)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+driver = webdriver.Chrome('./chromedriver.exe')
+driver.get("https://linkedin.com")
