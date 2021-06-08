769ed38cb6c900ea5e3a3eeb47ec4b02bba8364c
Aaron  Parodi Paez <56941544+Parodi14@users.noreply.github.com>
2021-06-08 15:27:43 -0500

    Delete automatizacionlinkedin.py

--- automatizacionlinkedin.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-import time
-
-driver = webdriver.Chrome('./chromedriver.exe')
-driver.get("https://linkedin.com")
